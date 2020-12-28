61287cfb6e04afca1f79f60483a8cb5c90e35689
Agustin Navarro Galdon <xAgustin93@gmail.com>
2020-12-28 19:16:12 +0100

    Start

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7faecb7ffe01c05ac03977bc2a70656f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1974fb88-6927-466b-8d05-ba346f04adda'
+};
